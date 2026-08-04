7ac5b6d971ca3f15debe12852ed297907782aff9
liujinpeng597 <2419167441@qq.com>
2026-08-04 17:41:49 +0800

    BUG:sizeof被双引号包裹,vsc->vcx

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc,char* argv[]){
     std::cout << "========================================" << std::endl;
     //验证网络协议包头是否严格按照1字节对齐，如果对齐输出6Bytes
     std::cout << "[Init] MsgHeader size on network:"
-              << "sizeof(vsc::protocol::MsgHeader)" << " bytes."<<std::endl;
+              << sizeof(vcs::protocol::MsgHeader) << " bytes."<<std::endl;
     //TODO:初始化EpollReactor,启动循环;
 
 }
